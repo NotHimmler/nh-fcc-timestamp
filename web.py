5a06f731fb1be445433f074c195bb094a7bc3d2c
Heinrich Malan <heinrichmalan@hotmail.com>
2016-02-20 04:00:23 +0000

    Implemented functionality to parse natural date strings

--- web.py
+++ web.py
@@ -1,6 +1,8 @@
 import os
 import re
 import math
+import string
+import calendar
 from datetime import date
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 app = Flask(__name__)
@@ -15,15 +17,25 @@ def giveJSON(dateString):
 	regex = '[\D+]'
 	m = re.match(regex, dateString)
 	
-	natural = ""
-	timestamp = ""
+	natural = None
+	timestamp = None
+	
+	
 	
 	#Natural Date
 	if m:
-		return "Natural Date"
+		dateObj = isValidNaturalDate(dateString)
+		if dateObj:
+			timestamp = calendar.timegm(dateObj.timetuple())
+			natural = dateString
+		else:
+			natural = None
+			timestamp = None
 	#Unix Timestamp
 	else:
-		if int(dateString) > math.pow(2,32):
+		regex = "\s+"
+		match = re.search(regex,dateString)
+		if match or (int(dateString) > math.pow(2,32)):
 			natural = None
 			timestamp = None
 		else:
@@ -35,10 +47,39 @@ def giveJSON(dateString):
 		
 	return jsonify(object)
 		
+#Constructs a natural date string of the form "MONTH DAY, YEAR" from indexes and integers
 def naturalDateString(year,month,day):
 	months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 	dateString = str.format("{0} {1}, {2}",months[month-1], str(day), str(year))
 	return dateString
+	
+#Checks to see if a date string is of the format "MONTH DAY, YEAR" and if that date is valid
+#If so, returns a date object, else returns false
+def isValidNaturalDate(dateString):
+	months = ['JANUARY', 'FEBRUARY', 'MARCH', 'APRIL', 'MAY', 'JUNE', 'JULY', 'AUGUST', 'SEPTEMBER', 'OCTOBER', 'NOVEMBER', 'DECEMBER']
+	days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+	
+	regex = "[A-Za-z]{1,10}\s\d{1,2},\s\d{4}"
+	match = re.search(regex,dateString)
+	if match:
+		month = re.search("[A-Za-z]{1,10}",dateString).group(0)
+		monthIndex = -1
+		try:
+			monthIndex = months.index(string.upper(month))+1
+		except:
+			return False
+		
+		day = re.search("\d{1,2}",dateString).group(0)
+		
+		year = re.search("\d{4}",dateString).group(0)
+		
+		try:
+			return date(int(year), monthIndex,int(day))
+		except:
+			return False
+		
+	else:
+		return False
 		
 if __name__ == '__main__':
 	app.secret_key = "super-secret-key"
